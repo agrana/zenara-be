70c8afbcc35c8afaf32489d3bc58b54ea4d82550
Alfonso Grana <alfonso.grana@gmail.com>
2025-05-13 21:55:59 +0200

    Adding vite variables

--- client/src/lib/supabaseClient.ts
+++ client/src/lib/supabaseClient.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 console.log('Environment variables:', {
-  SUPABASE_URL: import.meta.env.SUPABASE_URL,
-  SUPABASE_ANON_KEY: import.meta.env.SUPABASE_ANON_KEY
+  SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
+  SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY
 });
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Missing environment variables:', {
